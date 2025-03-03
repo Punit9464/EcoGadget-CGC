bea5f7678953b04212a71936f71c01ee1c54248e
Punit9464 <punitkumar8728@gmail.com>
2025-03-03 23:50:20 +0530

    final push

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
 
 app.use(
   expressSession({
